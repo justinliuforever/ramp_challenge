5662ce555e531c2a26e66bdaa4323e517e4f4bde
Justin Liu <77476135+justinliuforever@users.noreply.github.com>
2023-08-13 20:41:59 +0000

    fix bug six

--- src/App.tsx
+++ src/App.tsx
@@ -91,17 +91,19 @@ export function App() {
         <div className="RampGrid">
           <Transactions transactions={transactions} />
 
-          {transactions !== null && (
-            <button
-              className="RampButton"
-              disabled={paginatedTransactionsUtils.loading}
-              onClick={async () => {
-                await loadAllTransactions();
-              }}
-            >
-              View More
-            </button>
-          )}
+          {transactions !== null &&
+            !transactionsByEmployee &&
+            paginatedTransactions?.nextPage && (
+              <button
+                className="RampButton"
+                disabled={paginatedTransactionsUtils.loading}
+                onClick={async () => {
+                  await loadAllTransactions();
+                }}
+              >
+                View More
+              </button>
+            )}
         </div>
       </main>
     </Fragment>
